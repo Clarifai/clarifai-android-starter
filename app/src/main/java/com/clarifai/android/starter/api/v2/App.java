c012eace86772c4371f2e19dcab795e86e20b945
Kevin Most <kevin@kevinmost.com>
2016-10-12 11:45:48 -0400

    Enable logging!

--- app/src/main/java/com/clarifai/android/starter/api/v2/App.java
+++ app/src/main/java/com/clarifai/android/starter/api/v2/App.java
@@ -3,9 +3,9 @@ package com.clarifai.android.starter.api.v2;
 import android.app.Application;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-
 import clarifai2.api.ClarifaiBuilder;
 import clarifai2.api.ClarifaiClient;
+import timber.log.Timber;
 
 public class App extends Application {
 
@@ -28,6 +28,7 @@ public class App extends Application {
     INSTANCE = this;
     client = new ClarifaiBuilder(getString(R.string.clarifai_id), getString(R.string.clarifai_secret)).buildSync();
     super.onCreate();
+    Timber.plant(new Timber.DebugTree());
   }
 
   @NonNull
